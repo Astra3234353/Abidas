d403af998198c9e64b6e8e77860c1dba682f25d7
Astra <xc0rpi0n145.com@gmail.com>
2024-12-30 20:33:04 -0600

    Add styles for search engine

--- scripts/Abidas-store.js
+++ scripts/Abidas-store.js
@@ -3,5 +3,5 @@ import { renderGridItems, addGridItemTest } from "./products/load-product.js";
 renderGridItems();
 // addGridItemTest(20);
 
-console.log('Did you lost anything? xD');
-console.log('By: Astra.dev :)');
+console.log('Have you lost something? ;0');
+console.log('By: Astra.dev');
